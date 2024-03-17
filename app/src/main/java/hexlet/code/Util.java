7a0d2ff65066f99987759c56d39200ff682da123
HKreoin <steamuser85@yandex.ru>
2024-03-17 15:48:42 +0300

    modify Util class

--- app/src/main/java/hexlet/code/Util.java
+++ app/src/main/java/hexlet/code/Util.java
@@ -4,30 +4,39 @@ import java.util.Scanner;
 
 public class Util {
     public static int random() {
+
         int min = 1;
         int max = 100;
+
         return random(min, max);
     }
 
     public static int random(int min, int max) {
+
         int randomNumber = (int) (min + Math.random() * (max - min + 1));
+
         return randomNumber;
     }
 
     public static String operand() {
+
         int random = random(0, 2);
         String[] operands = {"+", "-", "*"};
+
         return operands[random];
     }
 
     public static int gsd(int num1, int num2) {
+
         int min = Math.min(num1, num2);
         int max = Math.max(num1, num2);
+
         while (max % min != 0) {
             int buffer = min;
             min = max % min;
             max = buffer;
         }
+
         return min;
     }
 
@@ -40,49 +49,66 @@ public class Util {
         int append = random(minStep, maxStep);
         int minNumber = 0;
         int maxNumber = 100;
-        numbers[0] = random(minNumber, maxNumber);
         int length = numbers.length;
+
+        numbers[0] = random(minNumber, maxNumber);
 
         for (int i = 1; i < length; i++) {
             numbers[i] = numbers[i - 1] + append;
         }
+
         return numbers;
     }
 
     public static String[] numsToStringArray(int[] numbers) {
+
         int length = numbers.length;
         String[] newArray = new String[length];
+
         for (int i = 0; i < length; i++) {
             newArray[i] = String.valueOf(numbers[i]);
         }
+
         return newArray;
     }
 
     public static void printArray(String[] array) {
+
         int length = array.length;
+
         for (int i = 0; i < length; i++) {
             System.out.print(array[i] + " ");
         }
+
         System.out.println();
     }
 
     public static String answer() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.print("Your answer: ");
         String answer = scanner.next();
+
         return answer;
     }
 
     public static boolean prime(int num) {
-        for (int i = 1; i <= Math.sqrt(num); i++) {
-            if (num % i == 0 || num <= 1) {
+
+        if (num <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
-        return true;
 
+        return true;
     }
+
     public static boolean check(String value1, String value2) {
+
         if (value1.equals(value2)) {
             System.out.println("Correct!");
             return true;
